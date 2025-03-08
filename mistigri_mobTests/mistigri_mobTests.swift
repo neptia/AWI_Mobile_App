db3d542f05f45a137e1e70288a828f1bee1b37c1
Neptia <poomedyrungen@gmail.com>
2025-03-08 17:34:10 +0100

    Initial Commit

---
+++ mistigri_mobTests/mistigri_mobTests.swift
@@ -0,0 +1,17 @@
+//
+//  mistigri_mobTests.swift
+//  mistigri_mobTests
+//
+//  Created by Poomedy Rungen on 08/03/2025.
+//
+
+import Testing
+@testable import mistigri_mob
+
+struct mistigri_mobTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
