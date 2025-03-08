fe08901b6ae8863239e63b5c4921a66eea548980
Neptia <poomedyrungen@gmail.com>
2025-03-08 17:34:10 +0100

    Initial Commit

---
+++ mistigri_mob/mistigri_mobApp.swift
@@ -0,0 +1,17 @@
+//
+//  mistigri_mobApp.swift
+//  mistigri_mob
+//
+//  Created by Poomedy Rungen on 08/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct mistigri_mobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
